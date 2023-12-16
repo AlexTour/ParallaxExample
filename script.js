425b785cd8ce508ec5a5abe1ece289d833cd273e
Alexandros Touramanis <32987136+AlexTour@users.noreply.github.com>
2023-12-16 13:49:48 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,43 @@
-const imageContainer = document.querySelector('.image-container');
-const photo = document.querySelector('img.photo');
-const depthMap = document.querySelector('img.depth-map');
+function createAnimatedLoop(photo, depthMap, loopDuration) {
+  // Create a canvas and context
+  const canvas = document.createElement('canvas');
+  canvas.width = photo.width;
+  canvas.height = photo.height;
+  const ctx = canvas.getContext('2d');
 
-const generateParallaxEffect = () => {
-    const perspective = 500;
-    const depthMapData = depthMap.getContext('2d').getImageData(0, 0, photo.width, photo.height);
+  // Create a data structure to store the frames
+  const frames = [];
 
+  // Load the photo and depth map
+  ctx.drawImage(photo, 0, 0);
+  const depthMapData = ctx.getImageData(0, 0, photo.width, photo.height);
+
+  // Calculate the perspective
+  const perspective = 500;
+
+  // Create the animated loop
+  for (let i = 0; i <= loopDuration; i++) {
+    // Create a new frame
+    const frame = document.createElement('img');
+    frame.src = canvas.toDataURL('image/png');
+
+    // Calculate the offset for each pixel in the photo
     for (let x = 0; x < photo.width; x++) {
-        for (let y = 0; y < photo.height; y++) {
-            const depthValue = depthMapData.data[(y * photo.width + x) * 4 + 3];
-            const parallaxDistance = perspective * depthValue / 255;
+      for (let y = 0; y < photo.height; y++) {
+        const depthValue = depthMapData.data[(y * photo.width + x) * 4 + 3];
+        const parallaxDistance = perspective * depthValue / 255;
 
-            photo.style.left = x - parallaxDistance + 'px';
-        }
+        ctx.fillStyle = 'rgba(255, 255, 255, 1)';
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+        ctx.lineTo(x - parallaxDistance, y);
+        ctx.fill();
+      }
     }
-};
 
-imageContainer.addEventListener('load', generateParallaxEffect);
+    // Add the frame to the frames array
+    frames.push(frame);
+  }
+
+  return frames;
+}
